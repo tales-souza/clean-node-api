5874904393501f071619cbd40b116fc68637cd3c
Tales  Monteiro <=>
2021-02-02 17:44:15 -0300

    test: ensure SignUpController return an error if no email is provided

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -6,7 +6,7 @@ describe('SignUp Controller', () => {
       body: {
         email:'tales.monteiro@hotmail.com',
         password:'tales123',
-        passwordConfirmation: 'tales123' 
+        passwordConfirmation: 'tales123',
       }
     }
 
@@ -14,4 +14,21 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Mising param: name'))
   })
+
+  test('Should return 400 if no email is provide ', ()  => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        password:'tales123',
+        passwordConfirmation: 'tales123',
+        name:'teste' 
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Mising param: email'))
+  })
+
+
 })
